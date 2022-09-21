fdb1af2d3cb1b37020790d784a404ccd0be621ed
marielee14 <marielee14@gmail.com>
2022-09-21 20:59:08 +0900

    add  more animations

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function revealMyInfo(MyInfo, ClassName, Color) {
-  let htmlElement = document.querySelector(ClassName);
+function revealMyInfo(myInfo, className, color, animationName) {
+  let htmlElement = document.querySelector(className);
 
-  htmlElement.innerHTML = MyInfo;
+  htmlElement.innerHTML = myInfo;
   htmlElement.style.color = color;
+
+  htmlElement.style.animationDuration = "1s";
+
+  if (animationName === "bgFadeOut") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "slideIn") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "heartBeat") {
+    htmlElement.style.animationName = animationName;
+  }
+
   //console.log(htmlElement);
 
   // span[index].innerHTML = MyName;
